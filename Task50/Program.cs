8f0d11f5bddadd7079dec3f94bb52c5e2a781cba
Sergey Vorobev <ch4rlythec4t@gmail.com>
2022-08-31 20:34:14 +0300

    Task50 rework

--- Task50/Program.cs
+++ Task50/Program.cs
@@ -1,18 +1,6 @@
 ﻿using System;
 class Program
 {
-    static double[,] CreateMatrix(int row, int column, double maxValue, double minValue)
-    {
-        double[,] newMatrix = new double[row, column];
-        for (int i = 0; i < row; i++)
-        {
-            for (int j = 0; j < column; j++)
-            {
-                newMatrix[i, j] = Math.Round((double)new Random().NextDouble() * maxValue - minValue, 1);
-            }
-        }
-        return newMatrix;
-    }
     static void WriteMatrix(double[,] matrix)
     {
         for (int i = 0; i < matrix.GetLength(0); i++)
@@ -24,31 +12,25 @@ class Program
             Console.WriteLine();
         }
     }
-    static void SearchPositionElement (double[,] matrix, int position)
+    static void SearchPositionElement(double[,] matrix, int position)
     {
-       int positionRow = position / 10;
-       int positionColumn = position % 10;
+        int positionRow = position / 10 - 1;
+        int positionColumn = position % 10 - 1;
 
-       if ((positionRow < matrix.GetLength(0))&&(positionColumn < matrix.GetLength(1))) Console.WriteLine(matrix[positionRow, positionColumn]);
-       else Console.WriteLine("Такого числа в массиве нет");
+        if ((positionRow < matrix.GetLength(0)) && (positionColumn < matrix.GetLength(1))) Console.WriteLine(matrix[positionRow, positionColumn]);
+        else Console.WriteLine("Такого числа в массиве нет");
     }
     static void Main()
     {
-        Console.Write("Введите количество столбцов массива M = ");
-        int M = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Введите количество строк массива N = ");
-        int N = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Введите минимальное значение для чисел Min = ");
-        double min = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Введите максимальное значение для чисел Max = ");
-        double max = Convert.ToInt32(Console.ReadLine());
-        double[,] newMatrix = CreateMatrix(row: M, column: N, maxValue: max, minValue: min);
+        double[,] newMatrix = {{1,4,7,2},
+                               {5,9,2,3},
+                               {8,4,2,4}};
 
         WriteMatrix(newMatrix);
 
         Console.Write("Введите позицию искомого элемента:  ");
         int position = Convert.ToInt32(Console.ReadLine());
 
-        SearchPositionElement (matrix: newMatrix, position: position);
+        SearchPositionElement(matrix: newMatrix, position: position);
     }
 }
